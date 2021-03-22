dbee44b742745a643813195b945ab11f67605899
0-Pirate-0 <i25Pirate@yandex.rum>
2021-03-22 20:13:19 +1000

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
     const windowWidth = $(window).width();
+    console.log(windowWidth)
     var header = $('.header');
     var headerLogo = $('.header__logo');
     //Header-fixed
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 1) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
             header.addClass('fixed-top');
         } else {
             header.removeClass('fixed-top')
         }
     });
     //Header-logo
-    if(windowWidth <= 960) {
+    if (windowWidth <= 960) {
         headerLogo.addClass('active')
     } else {
         headerLogo.removeClass('active')
     }
     //Nav-toggle
-    $('.nav_toggle').click(function(event){
+    $('.nav_toggle').click(function (event) {
         $('.nav_toggle, .nav').toggleClass('active');
     });
-     /* Collapse */
-     $("[data-collapse]").on("click", function(event) {
-         event.preventDefault();
-         
-         var $this = $(this), 
-             blockId = $this.data("collapse");
-         
-         $this.toggleClass("active");
-     }); 
+    /* Scrool */
+    $('[href^="#"]').on('click', function (event) {
+        if ($(this).attr('hash') !== "") {
+            event.preventDefault();
+            let hash = $(this).prop('hash');
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {});
+        }
+    });
     // Work
-    if(windowWidth <= 960 & windowWidth >= 640) {
+    if (windowWidth <= 960 & windowWidth >= 640) {
         $('.work__block--6 > img').attr('src', 'assets/images/details/6-1.jpg');
     }
+    // Slider
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 700,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 });
