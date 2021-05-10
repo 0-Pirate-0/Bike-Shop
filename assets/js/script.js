cd5134073bada70abd82da9a741707ebfe2819ea
0-Pirate-0 <i25Pirate@yandex.rum>
2021-05-10 12:05:47 +1000

    fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     const windowWidth = $(window).width();
-    console.log(windowWidth)
     var header = $('.header');
     var headerLogo = $('.header__logo');
+
     //Header-fixed
     $(window).scroll(function () {
         if ($(this).scrollTop() > 1) {
@@ -11,30 +11,35 @@ $(document).ready(function () {
             header.removeClass('fixed-top')
         }
     });
+
     //Header-logo
     if (windowWidth <= 960) {
         headerLogo.addClass('active')
     } else {
         headerLogo.removeClass('active')
     }
+
     //Nav-toggle
     $('.nav_toggle').click(function (event) {
         $('.nav_toggle, .nav').toggleClass('active');
     });
+
     /* Scrool */
     $('[href^="#"]').on('click', function (event) {
         if ($(this).attr('hash') !== "") {
             event.preventDefault();
             let hash = $(this).prop('hash');
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - header.innerHeight()
             }, 800, function () {});
         }
     });
+
     // Work
     if (windowWidth <= 960 & windowWidth >= 640) {
         $('.work__block--6 > img').attr('src', 'assets/images/details/6-1.jpg');
     }
+
     // Slider
     $('.slider__inner').slick({
         dots: true,
